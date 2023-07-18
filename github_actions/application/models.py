81c89aa07b306caf4bb880b33ca935e4efa2ddb9
jiminsong490 <misosjm@naver.com>
2023-07-19 01:07:01 +0900

    adq

--- github_actions/application/models.py
+++ github_actions/application/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Question(models.Model):
     title = models.CharField(max_length=30) # 제목
     content = models.CharField(max_length=100) # 내용
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30,default='')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
