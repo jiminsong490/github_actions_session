90b9a777d1374993bba11092f029c7b5d5535640
jiminsong490 <misosjm@naver.com>
2023-07-19 07:00:00 +0900

    feat : name 추가

--- github_actions/application/serializer.py
+++ github_actions/application/serializer.py
@@ -5,5 +5,5 @@ class QuestionSerializer(serializers.ModelSerializer):
 # 모델을 JSON 형태로 변환
     class Meta:
         model = Question # 모델 설정
-        fields = ('id','title','content','created_at', 'updated_at') # json으로 만들어 줄 필드
-        # fields = ('id','title','content','name','created_at', 'updated_at')
+        # fields = ('id','title','content','created_at', 'updated_at') # json으로 만들어 줄 필드
+        fields = ('id','title','content','name','created_at', 'updated_at')
